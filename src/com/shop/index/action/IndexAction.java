372ac01e3f352f0eb1458a5956905d5ee167d9de
wangmyhome <1131207997@qq.com>
2019-04-21 09:14:24 +0800

    分类模块

--- src/com/shop/index/action/IndexAction.java
+++ src/com/shop/index/action/IndexAction.java
@@ -1,7 +1,12 @@
 package com.shop.index.action;
 
 
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import com.shop.category.pojo.Category;
+import com.shop.category.service.CategoryService;
 
 
 /**
@@ -11,10 +16,20 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class IndexAction extends ActionSupport {
 
+	// 注入一级分类的Service:
+	private CategoryService categoryService;
+	
+	public void setCategoryService(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
 	/**
 	 * 执行的访问首页的方法:
 	 */
 	public String execute(){
+		// 查询所有一级分类集合
+		List<Category> cList = categoryService.findAll();
+		// 将一级分类存入到Session的范围:
+		ActionContext.getContext().getSession().put("cList", cList);
 		return "index";
 	}
 }
