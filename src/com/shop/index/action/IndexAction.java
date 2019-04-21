574cafb3f0db58951631eb151fe9983f8e7bece2
wangmyhome <1131207997@qq.com>
2019-04-21 09:55:31 +0800

    首页查询最热最新商品

--- src/com/shop/index/action/IndexAction.java
+++ src/com/shop/index/action/IndexAction.java
@@ -7,6 +7,8 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.shop.category.pojo.Category;
 import com.shop.category.service.CategoryService;
+import com.shop.product.pojo.Product;
+import com.shop.product.service.ProductService;
 
 
 /**
@@ -18,9 +20,14 @@ public class IndexAction extends ActionSupport {
 
 	// 注入一级分类的Service:
 	private CategoryService categoryService;
+	// 注入商品的Service
+	private ProductService productService;
 	
 	public void setCategoryService(CategoryService categoryService) {
 		this.categoryService = categoryService;
+	}
+	public void setProductService(ProductService productService) {
+		this.productService = productService;
 	}
 	/**
 	 * 执行的访问首页的方法:
@@ -30,6 +37,14 @@ public class IndexAction extends ActionSupport {
 		List<Category> cList = categoryService.findAll();
 		// 将一级分类存入到Session的范围:
 		ActionContext.getContext().getSession().put("cList", cList);
+		// 查询热门商品:
+		List<Product> hList = productService.findHot();
+		// 保存到值栈中:
+		ActionContext.getContext().getValueStack().set("hList", hList);
+		// 查询最新商品:
+		List<Product> nList = productService.findNew();
+		// 保存到值栈中:
+		ActionContext.getContext().getValueStack().set("nList", nList);
 		return "index";
 	}
 }
